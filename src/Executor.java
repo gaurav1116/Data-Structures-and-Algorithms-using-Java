6fe16dde0813f0089eae0fb75b2022b27cd9f0e9
gaurav1116 <gaurav.1116@outlook.com>
2021-11-01 18:13:35 +0530

    LinkedList Package implemented and pushed.

---
+++ src/Executor.java
@@ -0,0 +1,14 @@
+
+public class Executor {
+
+	public static void main(String[] args) {
+
+		LinkedList list = new LinkedList();
+		list.insert(5);
+		list.insert(20);
+		list.insert(35);
+		
+		list.show();
+	}
+
+}
